846bc3de7008c565fb93930e83ad18986a7ee638
Sergey Abramov <aswq22013@gmail.com>
2022-06-12 18:18:59 +0300

    fixes

--- ip_app/models/general_models.py
+++ ip_app/models/general_models.py
@@ -32,6 +32,12 @@ class User(db.Model):
     birth_date = db.Column(db.Date)
     password_hash = db.Column(db.String(100))
 
+    taught_courses = db.relationship(
+        'courses',
+        secondary='course_teachers',
+        back_populates='taught_courses'
+    )
+
 
 class Course(db.Model):
     __tablename__ = 'courses'
@@ -45,7 +51,7 @@ class Course(db.Model):
     teachers = db.relationship(
         User,
         secondary='course_teachers',
-        backref='taught_courses'
+        back_populates='taught_courses'
     )
 
 
