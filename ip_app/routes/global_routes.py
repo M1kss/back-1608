17208cd1028fe77281f2643a6f64869ad4355881
Sergey Abramov <aswq22013@gmail.com>
2022-06-12 16:50:35 +0300

    debug

--- ip_app/routes/global_routes.py
+++ ip_app/routes/global_routes.py
@@ -378,7 +378,7 @@ class CourseCollection(Resource, PaginationMixin):
         """
         return self.paginate(pagination_parser.parse_args())
 
-    @api.expect(course_post_model, course_pic_parser)
+    @api.expect(course_post_model)
     @api.marshal_with(course_full_model)
     @api.response(403, 'Access denied')
     @api.response(404, 'Author not found')
