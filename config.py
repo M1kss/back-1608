61b444f6bbdf3002ee85788b8eb22f83e32582f8
Sergey Abramov <aswq22013@gmail.com>
2022-06-17 21:28:48 +0300

    fixes

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config(object):
     SQLALCHEMY_DATABASE_URI = 'mysql://iproffi:IPROFFI86@localhost:3306/iproffi?charset=utf8'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    RESTPLUS_VALIDATE = True
+    RESTX_VALIDATE = True
     LOGGER_LEVEL = 'DEBUG'
     MAX_CONTENT_LENGTH = 2 * 1024 * 1024
     EXECUTOR_MAX_WORKERS = 1
